e5b477bfed0859068f5f9c99fc3ee5e715cc83c1
patrickatcw <pj8392@yahoo.com>
2018-05-10 19:05:10 -0400

    addded apple and peaches to list, peaches to a specific location

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -1,8 +1,35 @@
 package com.me;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+//https://kodejava.org/how-do-i-convert-an-array-into-a-collection-object//
 public class Main {
 
     public static void main(String[] args) {
+
+        ArrayList<String> sample = new ArrayList<String>();
+        String[] arrayList = new String[]{"Banana", "Prunes", "Grapes"};
+        sample.add("Apple");
+        sample.add(0, "Peaches");
+        sample.addAll(1, Arrays.asList(arrayList));
+        System.out.println("Number of elements = " + sample.size());
+        System.out.println("-----------------------------------------");
+
+        for (int index = 0; index < sample.size(); index++) {
+
+            System.out.println(sample.get(index));
+
+            /*
+            Number of elements = 6
+            Peaches
+            Banana
+            Prunes
+            Grapes
+            Apple
+             */
+
+        }
 
     }
 
